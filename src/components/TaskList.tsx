af7d1ccec1e843e3de12cf240dd03fa027a59c78
Fabricio Souza <compifab@gmail.com>
2021-08-30 20:04:56 -0300

    Completed challenge tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import {v4 as uuid} from 'uuid'
+
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
@@ -16,14 +18,27 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== '') {
+      setTasks([...tasks, { 
+        id: uuid(),
+        title: newTaskTitle,
+        isComplete: false
+      }])
+
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      return task.id === id ? {...task, isComplete: !task.isComplete} : task;
+    }))
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => (task.id !== id)))
   }
 
   return (
